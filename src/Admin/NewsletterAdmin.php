a1dc988fb3c6faf471119eb356d038f7b63ff10b
harold <wtakoutsing@gmail.com>
2019-03-17 16:30:17 +0100

    ajout de admin

---
+++ src/Admin/NewsletterAdmin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+final class NewsletterAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+			->add('id')
+			->add('email')
+			;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+			->add('id')
+			->add('email')
+			->add('_action', null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+			->add('id')
+			->add('email')
+			;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+			->add('id')
+			->add('email')
+			;
+    }
+}
---
+++ src/Controller/MessageAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class MessageAdminController extends CRUDController
+{
+
+}
