d6c099f95edbe3fd01cfe732da2d1e7da7a0764c
harold <wtakoutsing@gmail.com>
2019-05-01 11:00:11 +0100

    langue

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends Controller
@@ -10,15 +16,34 @@ class IndexController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
-         $em = $this->getDoctrine()->getManager();
-          $temoignages = $em->getRepository('App:Temoignage')->findBy(array(), array('id' => 'DESC'));
-           $accueils = $em->getRepository('App:Accueil')->findAll();
+        $em = $this->getDoctrine()->getManager();
+        $temoignages = $em->getRepository('App:Temoignage')->findBy(array(), array('id' => 'DESC'));
+        $accueils = $em->getRepository('App:Accueil')->findAll();
+        $newsletter = new Newsletter();
+
+        $form = $this->createForm(NewsletterType::class,$newsletter);
+        if($request->getMethod() == "POST"){
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $news = $em->getRepository('App:Newsletter')->findOneByEmail($newsletter->getEmail());
+                if(!$news){
+                    $session = new Session();
+                    $session->set("newsletter",true);
+                    $em->persist($newsletter);
+                    $em->flush();
+                }
+            }
+        }
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
              "temoignages"=> $temoignages,
-             "accueils"=> $accueils
+             "accueils"=> $accueils,
+            "form"=>$form->createView()
         ]);
     }
 
@@ -42,4 +67,13 @@ class IndexController extends Controller
         return $this->render('partenaire.html.twig',["partenaires"=> $partenaires]);
     }
 
+    /**
+     * @Route("/changelocale/{id}", name="changelocale")
+     */
+    public function changelocale(Request $request, $id = 'fr'){
+
+        $request->setLocale($id);
+        $url = $this->generateUrl('index',['_locale' => $id]);
+        return $this->redirect($url);
+    }
 }
